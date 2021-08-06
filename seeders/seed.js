d7c5d40180417ab0d1e133cf512f3c4469ef0998
kaivon tolooee <kaivont@gmail.com>
2021-08-06 01:52:11 -0400

    trying to deploy again to heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workoutTracker', {
+mongoose.connect('mongodb://localhost/workoutTracker', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 const workoutSeed = [
